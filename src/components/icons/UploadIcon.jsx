0e4752444079e2cdc67276d93ec47112a3b1b37b
Shoji <hshojikato@gmail.com>
2024-11-22 13:01:25 -0300

    refactor: icons

--- src/components/icons/UploadIcon.jsx
+++ src/components/icons/UploadIcon.jsx
@@ -1,5 +1,5 @@
-export const UploadIcon = (props) => (
-  <svg
+export default function UploadIcon(props) {
+  return <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
@@ -13,4 +13,4 @@ export const UploadIcon = (props) => (
       d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
     />
   </svg>
-);
+}
