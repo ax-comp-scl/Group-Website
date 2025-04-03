df901ba10731c27607ca91906264d227da5d2306
professor <agiampedro@gmail.com>
2025-04-03 21:03:21 +0100

    refactor: page contact admin

--- src/pages/ContactAdm.jsx
+++ src/pages/ContactAdm.jsx
@@ -15,11 +15,11 @@ const loginSchema = z.object({
   password: z.string().min(5, "A senha deve ter pelo menos 5 caracteres"),
 });
 
-export default function ContactAdm() {
+export default function LoginPage() {
   const [isVisible, setIsVisible] = useState(false);
   const toggleVisibility = () => setIsVisible(!isVisible);
   const navigate = useNavigate();
-  const [loginErrorMessage, setLoginErrorMessage] = useState('');
+  const [loginErrorMessage, setLoginErrorMessage] = useState('')
 
   const {
     control,
@@ -29,42 +29,49 @@ export default function ContactAdm() {
     resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
-      text: "",
+      password: "",
     },
   });
 
   const onSubmit = async (data) => {
-    
+    try {
+      const user = await loginUser(data.email, data.password);
+      if (user) {
+        navigate("/admin/history");
+      }
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      setLoginErrorMessage('Credenciais inválidas')
+
+      setTimeout(() => {
+        setLoginErrorMessage('')
+      }, 1500)
+    }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
-      <div className="w-full max-w-2xl bg-white shadow-2xl rounded-2xl overflow-hidden flex flex-col flex items-center justify-center " 
-      style={{ minHeight: '600px' }}>
-        
-        <div className="w-full h-20">
-          <img
-            src={loginImg}
-            alt="Login header"
-            className="w-full h-full object-cover"
-          />
-        </div>
+    <div className="flex items-center justify-center min-h-screen bg-emerald-50">
+      <div className="flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 ">
+        <div className="flex flex-col justify-center p-8 md:p-14">
 
-        
-        <div className="p-8">
-          
-          <div className="flex justify-center mb-6">
+          <div className="button-back">
+            <a href="/">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>
+            </a>
+          </div>
+          <div className="h-[100px] mb-4">
             <img
               src={logoEmbrapa}
-              alt="Embrapa Logo"
-              className="h-16 object-contain "
+              alt="img"
+              className="h-full hidden rounded-r-2xl md:block object-cover"
             />
           </div>
-
-          
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 ">
-          <div className="py-4">
-              <span className="mb-2 text-md">E-mail</span>
+          <span className="mb-8 font-light">
+            Contate um Admin
+          </span>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="py-4">
+              <span className="mb-2 text-md">E-mail para entrarmos em contato</span>
               <Controller
                 name="email"
                 control={control}
@@ -74,40 +81,50 @@ export default function ContactAdm() {
                     placeholder="Seu e-mail"
                     variant="bordered"
                     radius="sm"
-                    className="w-80"
                     isClearable
+                    color={errors.email ? "danger" : "success"}
+                    isInvalid={!!errors.email}
                     errorMessage={errors.email?.message}
                   />
                 )}
               />
             </div>
-
             <div className="py-4">
-                <Controller
-                name="text"
+              <label for="message" class="mb-2 text-md">Sua mensagem</label>
+              <Controller
+                name="password"
                 control={control}
-                render={({field})=>(
-                    <textarea 
-                    variant="bordered" 
-                    className="mb-2 p-4 text-md w-80 h-40 focus:outline-none border-2 border-gray-300"
-                    placeholder="Adicione o motivo de nos contatar..." 
-                    class="textarea 
-                    textarea-md">
+                render={({ field }) => (
+                  <textarea id="message" rows="4" class="block 
+                  p-2.5 w-full text-sm text-gray-900 bg-gray-50 
+                  rounded-lg border border-gray-300 focus:ring-blue-500 
+                  focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 
+                  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  placeholder="Digite aqui...">
 
-                    </textarea>
+                  </textarea>
                 )}
-                    
-                />
+              />
             </div>
-
-            {loginErrorMessage && (
-              <p className="text-red-500 text-sm">{loginErrorMessage}</p>
-            )}
-
-            <Button type="submit" className="w-full accent-content bg-green-700 text-white">
-              Entrar
+            
+            <Button
+              type="submit"
+              color="default"
+              variant="solid"
+              radius="small"
+              className="text-lg bg-black text-white rounded-lg mb-6 p-2 w-full  hover:bg-green-900 hover:text-white hover:border hover:border-gray-300"
+            >
+              Enviar
             </Button>
           </form>
+          
+        </div>
+        <div className="relative w-[350px]">
+          <img
+            src={loginImg}
+            alt="img"
+            className="h-full hidden rounded-r-2xl md:block object-cover"
+          />
         </div>
       </div>
     </div>
