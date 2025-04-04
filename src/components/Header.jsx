99350bee5ba5fdfbd1e0fc694cfe202da01316d9
matheusspacifico <matheuspacifico44@gmail.com>
2025-04-03 21:45:53 -0300

    style(Header): centered navbar
    Co-authored-by: DylanPetrucelli <DylanPetrucelli@users.noreply.github.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import Navbar from "./Navbar";
 import { Tab } from "@nextui-org/react";
 
 export default function Header() {
-  const user = getUser()
+  const user = getUser();
 
   const admOptions = [
     { key: "/admin/history", title: "Histórico" },
@@ -17,9 +17,7 @@ export default function Header() {
     { key: "/organisms", title: "Listar dados" },
   ];
 
-  const userOptions = [
-    { key: "/organisms", title: "Listar dados" },
-  ];
+  const userOptions = [{ key: "/organisms", title: "Listar dados" }];
 
   const tabOptions = (user.is_staff ? admOptions : userOptions).map((tab) => (
     <Tab key={tab.key} title={tab.title} />
@@ -27,19 +25,20 @@ export default function Header() {
 
   return (
     <div className="sticky top-0 bg-white/50 backdrop-blur-sm z-50">
-      <div className="navbar px-12 py-4 gap-4 ">
-        <div className="flex-1">
-          <div className="hidden md:block w-40">
-            <Link to={"/admin/history"}><img src={embrapa} alt="Logo da Embrapa" /></Link>
-          </div>
+      <div className="px-12 py-4 flex items-center justify-between">
+        <div className="w-40 hidden md:block">
+          <Link to={"/admin/history"}>
+            <img src={embrapa} alt="Logo da Embrapa" />
+          </Link>
         </div>
 
-        <Navbar
-          options={tabOptions}
-          base="w-full max-w-2xl"
-        />
+        <div className="flex-1 flex justify-center">
+          <Navbar options={tabOptions} base="w-full max-w-2xl" />
+        </div>
 
-        <AvatarComponent size="lg" />
+        <div className="w-40 flex justify-end">
+          <AvatarComponent size="lg" />
+        </div>
       </div>
       <Divider />
     </div>
