5b56170c023d324ee0b92c1b40a1f423f95eefe6
matheusspacifico <matheuspacifico44@gmail.com>
2025-05-22 19:40:17 -0300

    chore: add missing import

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -4,6 +4,7 @@ import { useDropzone } from 'react-dropzone'
 import FileTrashIcon from './icons/FileTrashIcon'
 import FileUploadedIcon from './icons/FileUploadedIcon'
 import UploadIcon from './icons/UploadIcon'
+import { axios } from '../lib/axios'
 
 export default function Dropzone(props) {
   const [rejectedFiles, setRejectedFiles] = useState([])
